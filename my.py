e9c21f3285188f73281379b2197acf112a5f548d
jeniffermukami <mukamijeniffer6@gmail.com>
2022-06-01 13:59:39 +0300

    initial commit

---
+++ my.py
@@ -0,0 +1,8 @@
+import tensorflow
+from tensorflow import keras
+from keras import models
+from keras import layers
+from keras  import optimizers 
+from keras.models import Sequential
+from keras.layers import Dense,Activation,Dropout 
+from keras.optimizers import SGD
